88f45b8adf8c48004ab4e61b82452f913f5ff97d
pablosky5 <pablosky96futbol@hotmail.com>
2017-10-10 18:59:09 +0200

    Clase carrera con atributos

--- Proyecto/src/logica/Carrera.java
+++ Proyecto/src/logica/Carrera.java
@@ -1,5 +1,74 @@
 package logica;
 
+import java.sql.Date;
+
 public class Carrera {
+	private String nombre;
+	private double precio;
+	private Date fechaFinalizaInscripcion;
+	private Date fechaEmpiezaInscripcion;
+	private Date fechaCompeticion;
+	private double distancia;
+	
+	public Carrera(String nombre,double precio,Date fechaFI,Date fechaEI, Date fechaCompe,double distancia){
+		
+		this.nombre = nombre;
+		this.precio = precio;
+		this.fechaFinalizaInscripcion = fechaFI;
+		this.fechaEmpiezaInscripcion = fechaEI;
+		this.fechaCompeticion = fechaCompe;
+		this.distancia = distancia;	
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Date getFechaFinalizaInscripcion() {
+		return fechaFinalizaInscripcion;
+	}
+
+	public void setFechaFinalizaInscripcion(Date fechaFinalizaInscripcion) {
+		this.fechaFinalizaInscripcion = fechaFinalizaInscripcion;
+	}
+
+	public Date getFechaEmpiezaInscripcion() {
+		return fechaEmpiezaInscripcion;
+	}
+
+	public void setFechaEmpiezaInscripcion(Date fechaEmpiezaInscripcion) {
+		this.fechaEmpiezaInscripcion = fechaEmpiezaInscripcion;
+	}
+
+	public Date getFechaCompeticion() {
+		return fechaCompeticion;
+	}
+
+	public void setFechaCompeticion(Date fechaCompeticion) {
+		this.fechaCompeticion = fechaCompeticion;
+	}
+
+	public double getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	
+	
 
 }
